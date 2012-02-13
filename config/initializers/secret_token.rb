6a5af0249b5ad428e9e0fd7212e7c054aab3b6ad
root <root@ip-10-243-86-31.ec2.internal>
2012-02-13 12:53:23 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Helloworld::Application.config.secret_token = '16f5cb293512c6cfbc7f5a1162f3c45cb0b539c4d4a8ce29c7342020843668d11cf0be647a30b0d25dd0b70bd70e5463a6d3723b11aeacf0a703c1aaa8de7e6d'
---
+++ server_start.sh
@@ -0,0 +1,3 @@
+#! /bin/bash
+# script to start up the WEBrick server on port 80
+rails s -p 80 & > helloworld.log
